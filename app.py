dea3dcc72db8c3eac591589860b8ed8840b47413
pawhal <pawhal@st.amu.edu.pl>
2018-01-11 22:25:31 +0100

    added /myreceipt

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, jsonify, redirect, session
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import json
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://paw:1234@77.55.217.112/paw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '7d441f27d441f27567dasfagsfrgty3546t3y46fyd63'
+#app.config['SECRET_KEY'] = '7d441f27d441f27567dasfagsfrgty3546t3y46fyd63'
 db = SQLAlchemy(app)
 from models import *
 
@@ -16,38 +17,38 @@ def home_site():
     return render_template("index.html")
 
 
-@app.route("/logout")
-def logout():
-    if 'user' in session:
-        session.pop('user')
-    return redirect("/")
-
-
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/register", methods=['POST'])
 def register():
     if request.method == 'POST':
         data = request.json
-        username = data['username']
-        password = data['password']
+        creds = json.loads(data)
+
+        username = creds.get('username')
+        password = creds.get('password')
 
         password = generate_password_hash(password)
 
         user = User.query.filter_by(name=username).first()
 
         if user:
-            return redirect("/register")
+            return redirect("/register"),
 
         user = User(name=username, password=password)
 
         db.session.add(user)
         db.session.commit()
 
-        return redirect("/")
+        return redirect("/"), 583
 
 
-@app.route("/receipts", methods=['GET'])
-def receipts():
-    r = Receipt.query.all()
+@app.route("/myreceipts", methods=['GET', 'POST'])
+def myreceipts():
+    dataj = request.json
+    data = json.loads(dataj)
+    user = data.get('user')
+    print(user)
+    r = Receipt.query.filter(Receipt.user == user)
+
     return jsonify(receipts=[i.serialize for i in r])
 
 
@@ -55,9 +56,10 @@ def receipts():
 def login():
     if request.method == 'POST':
         data = request.json
+        creds = json.loads(data)
 
-        username = data['username']
-        password = data['password']
+        username = creds.get('username')
+        password = creds.get('password')
 
         user = User.query.filter_by(name=username).first()
 
@@ -65,7 +67,7 @@ def login():
             password_hash = user.password
 
             if check_password_hash(password_hash, password):
-                session['user'] = username
+                return redirect("/"), 583
 
     return redirect("/")
 
@@ -73,19 +75,21 @@ def login():
 @app.route("/addreceipt", methods=['POST'])
 def addreceipt():
     if request.method == 'POST':
-        user = session['user']
-        data = request.json
-        name = data['name']
-        category = data['category']
-        datetime = data['date']
-        cost = float(data['cost'])
+        dataj = request.json
+        data = json.loads(dataj)
+        user = data.get('user')
+        name = data.get('name')
+        category = data.get('category')
+        datetime = data.get('datetime')
+        cost = float(data.get('cost'))
+        print(datetime)
 
         new_receipt = Receipt(user=user, name=name, category=category, cost=cost, date=datetime)
 
         db.session.add(new_receipt)
         db.session.commit()
 
-        return redirect("/addreceipt")
+        return redirect("/addreceipt"), 583
 
 
 if __name__ == "__main__":
